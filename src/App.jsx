974f62f48e419c3e65e7659bd225948a24c0951f
Damian Byszewski <d.byszewski@binarapps.com>
2020-04-26 11:59:45 +0200

    initialize project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+const a = [
+  a
+];
+const App = () => (
+  <Button variant="contained" color="primary">
+    Hello World
+  </Button>
+
+);
+
+export default App;
